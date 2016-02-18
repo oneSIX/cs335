5a4a575c7b647ee250b7ed04bab0ea83b3d1f948
Kent <kent.sinclair@gmail.com>
2016-02-17 21:43:32 -0500

    prediction completed

--- bayes.py
+++ bayes.py
@@ -1,4 +1,6 @@
+from __future__ import division
 from Patient import Patient
+
 import csv
 
 
@@ -14,7 +16,7 @@ def build_array(file_to_open):
         print(e)
     return patient_array
 
-# helper methods used by model to filter patient
+# helper methods used by model to filter patients inside model
 def filter_gender(patients, value):
     for el in patients:
         if el.gender == value:
@@ -70,9 +72,10 @@ def filter_weight_light_negatives(patients):  # add virus stuff
 
 
 def model():
+    # The following wall of code uses the above helper methods to generate lists of the various types; females, males, with virus, without virus
+    # etc.
     data = []
     data = build_array("proj1train.txt")
-    
     patient_count = len(data)
     males = list(filter_gender(data, "male"))
     females = list(filter_gender(data, "female"))
@@ -82,96 +85,109 @@ def model():
     with_virus_count = len(with_virus_list)
     no_virus_list = list(filter_virus(data, "N"))
     no_virus_count = len(no_virus_list)
+
     male_virus_list = list(filter_gender_positives(data, "male"))
     female_virus_list = list(filter_gender_positives(data, "female"))
     female_positive_count = len(female_virus_list)
     female_negative_count = female_count - female_positive_count
-    
-
     positive_blood_with_virus = list(filter_blood_positives(data, "+"))
     positive_blood_with_virus_count = len(positive_blood_with_virus)
-    positive_blood_no_virus = list(filter_blood_negative(data, "+"))  # new
+    positive_blood_no_virus = list(filter_blood_negative(data, "+"))
     positive_blood_no_virus_count = len(positive_blood_no_virus)
     negative_blood_with_virus = list(filter_blood_positives(data, "-"))
     negative_blood_with_virus_count = len(negative_blood_with_virus)
-    negative_blood_no_virus = list(filter_blood_negative(data, "-"))  # new
+    negative_blood_no_virus = list(filter_blood_negative(data, "-"))
     negative_blood_no_virus_count = len(negative_blood_no_virus)
     heavy_weight_with_virus = list(filter_weight_heavy_positives(data))
     heavy_weight_with_virus_count = len(heavy_weight_with_virus)
-    heavy_weight_no_virus = list(filter_weight_heavy_negatives(data))  # new
+    heavy_weight_no_virus = list(filter_weight_heavy_negatives(data))
     heavy_weight_with_no_count = len(heavy_weight_no_virus)
     light_weight_with_virus = list(filter_weight_light_positives(data))
     light_weight_with_virus_count = len(light_weight_with_virus) 
-    light_weight_no_virus = list(filter_weight_light_negatives(data))  # new
+    light_weight_no_virus = list(filter_weight_light_negatives(data))
     light_weight_with_no_count = len(light_weight_no_virus)
 
-    # helper print statements 
-    
-    # print(str(len(males)) + " Males")
-    # print(str(len(females)) + " Females")
-    # print(str(len(with_virus_list)) + " Patients with virus")
-    # print(str(len(no_virus_list)) + " Patients without virus")
-    # print(str(len(male_virus_list)) + " Males with virus")
-    # print(str(len(female_virus_list)) + " Female with virus")
-    # print(str(positive_blood_with_virus_count) + " Positive blood type who have virus")
-    # print(str(negative_blood_with_virus_count) + " Negative blood type who have virus")
-    # print(str(heavy_weight_with_virus_count) + " Above 170LBS with virus")
-    # print(str(light_weight_with_virus_count) + " Equal or Below 170lbs with virus")
-    # print("\n\nStart of Model Calculations\n")
 
+    # calculating likelihoods using floating point division
     prior_not_virus = (float(no_virus_count) / float(patient_count))
-    print("prior for not virus: " + str(prior_not_virus))
-    
     prior_virus = (float(with_virus_count) / float(patient_count))
-    print("prior for with virus: " + str(prior_virus))
-    
     liklihood_gender_female_positive = (float(female_positive_count) / float(with_virus_count))
-    print("likelihood for female given virus: " + str(liklihood_gender_female_positive))
-    
     liklihood_gender_female_negative = (float(female_negative_count) / float(no_virus_count))
-    print("likelihood for female given not virus: " + str(liklihood_gender_female_negative))
-    
     liklihood_gender_male_positive = 1 - liklihood_gender_female_positive
-    print("likelikhood for male given virus: " + str(liklihood_gender_male_positive))
-    
     liklihood_gender_male_negatives = 1 - liklihood_gender_female_negative
-    print("likelikhood for male given not virus: " + str(liklihood_gender_male_negatives))
-
-
-
     liklihood_blood_positive_with_virus = (float(positive_blood_with_virus_count) / float(with_virus_count))
-    print("likelihood for blood positive given virus: " + str(liklihood_blood_positive_with_virus))
-    
     liklihood_negative_blood_with_virus = (float(negative_blood_with_virus_count) / float(with_virus_count))
-    print("likelikhood for blood negative given virus: " + str(liklihood_negative_blood_with_virus))
-
-
-# these are calculated wrong   new
     liklihood_positive_blood_no_virus = (float(positive_blood_no_virus_count) / float(no_virus_count))
-    print("likelikhood for blood positive given not virus: " + str(liklihood_positive_blood_no_virus))
-
     liklihood_negative_blood_no_virus = (float(negative_blood_no_virus_count / float(no_virus_count)))
-    print("likelikhood for blood negative given not virus: " + str(liklihood_negative_blood_no_virus))
-
-
-
-
     liklihood_for_weights_heavy_with_virus = (float(heavy_weight_with_virus_count) / float(with_virus_count))
-    print("likelihood for weight > 170 given virus: " + str(liklihood_for_weights_heavy_with_virus))
-    
-# wrong     
     liklihood_for_weights_heavy_no_virus = (float(heavy_weight_with_no_count) / float(no_virus_count))
-    print("likelihood for weight > 170 given not virus: " + str(liklihood_for_weights_heavy_no_virus))
-    
     liklihood_for_weights_light_with_virus = (float(light_weight_with_virus_count) / float(with_virus_count))
-    print("likelihood for weight <= 170 given virus: " + str(liklihood_for_weights_light_with_virus))
-
-# wrong
     liklihood_for_weights_light_no_virus = (float(light_weight_with_no_count) / float(no_virus_count))
+
+    print("prior for not virus: " + str(prior_not_virus))
+    print("prior for with virus: " + str(prior_virus))
+    print("likelihood for female given virus: " + str(liklihood_gender_female_positive))
+    print("likelihood for female given not virus: " + str(liklihood_gender_female_negative))
+    print("likelikhood for male given virus: " + str(liklihood_gender_male_positive))
+    print("likelikhood for male given not virus: " + str(liklihood_gender_male_negatives))
+    print("likelihood for blood positive given virus: " + str(liklihood_blood_positive_with_virus))
+    print("likelikhood for blood negative given virus: " + str(liklihood_negative_blood_with_virus))
+    print("likelikhood for blood positive given not virus: " + str(liklihood_positive_blood_no_virus))
+    print("likelikhood for blood negative given not virus: " + str(liklihood_negative_blood_no_virus))
+    print("likelihood for weight > 170 given virus: " + str(liklihood_for_weights_heavy_with_virus))
+    print("likelihood for weight > 170 given not virus: " + str(liklihood_for_weights_heavy_no_virus))
+    print("likelihood for weight <= 170 given virus: " + str(liklihood_for_weights_light_with_virus))
     print("likelihood for weight <= 170 given not virus: " + str(liklihood_for_weights_light_no_virus))
 
+    # I was going to make this pretty but I'm just going to do it all in this method.
+    NN = NY = YN = YY = 0 
 
+    line = 1
+    test = []
+    test = build_array("proj1test.txt")
+    test_with_virus_list = list(filter_virus(test, "Y"))
+    test_with_virus_count = len(test_with_virus_list)
+    test_no_virus_list = list(filter_virus(test, "N"))
+    test_no_virus_count = len(test_no_virus_list)
 
+    for n in test:
+        no_virus = test_no_virus_count
+        yes_virus = test_with_virus_count
+        if (n.gender == "male"):
+            no_virus *= liklihood_gender_male_negatives
+            yes_virus *= liklihood_gender_male_positive
+        else:
+            no_virus *= liklihood_gender_female_negative
+            yes_virus *= liklihood_gender_female_positive
+        if (n.blood.endswith("+")):
+            no_virus *= liklihood_positive_blood_no_virus
+            yes_virus *= liklihood_blood_positive_with_virus
+        else:
+            no_virus *= liklihood_negative_blood_no_virus
+            yes_virus *= liklihood_negative_blood_with_virus
+        if (n.weight > 170):
+            no_virus *= liklihood_for_weights_heavy_no_virus
+            yes_virus *= liklihood_for_weights_heavy_with_virus
+        else:
+            no_virus *= liklihood_for_weights_light_no_virus
+            yes_virus *= liklihood_for_weights_light_with_virus
+        if (yes_virus >= no_virus):
+            prediction = True
+        else:
+            prediction = False
+        if (n.virus == "Y"):
+            if (prediction):
+                YY += 1
+            else:
+                YN += 1
+        else:
+            if (prediction):
+                NY += 1
+            else:
+                NN += 1
+        actual = str(n.virus)
+        predicted = "Y" if prediction else "N"
+        print(str(line) + " " + actual + " " + predicted)
 
 model()
 
